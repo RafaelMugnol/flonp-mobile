9e8b3dd0c58a9387bb36f45169245809948aaae2
Rafael Mugnol <rafamugnol@gmail.com>
2020-03-02 22:56:02 -0300

    Melhora na recarga do token.

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ import { AsyncStorage } from 'react-native';
 import NavigationService from './navigationService';
 
 const baseURLApi = 'https://projemerc.azurewebsites.net/api';
+let obtendoToken = false;
 
 const api = axios.create({
   baseURL: baseURLApi,
@@ -21,16 +22,14 @@ api.interceptors.request.use(async (config) => {
 api.interceptors.response.use((response) => response, async (error) => {
   const originalRequest = error.config;
 
-  if (error.response.status === 401 && originalRequest.url
-    === `${baseURLApi}/AccountCliente/Login`) {
-    await AsyncStorage.setItem('accessToken', '');
-    NavigationService.navigate('Login');
-
-    return Promise.reject(error);
-  }
-
   if (error.response.status === 401 && !originalRequest._retry) {
+    if (obtendoToken)
+      return api(originalRequest);
+
+
     originalRequest._retry = true;
+    obtendoToken = true;
+
     const refreshTokenOld = await AsyncStorage.getItem('refreshToken');
     const email = await AsyncStorage.getItem('email');
 
@@ -41,18 +40,27 @@ api.interceptors.response.use((response) => response, async (error) => {
         email,
       })
       .then(async (res) => {
-        if (res.status === 200) {
+        if (res.status === 200 && res.data.authenticated) {
           const { accessToken, refreshToken } = res.data;
 
           await AsyncStorage.setItem('accessToken', accessToken);
           await AsyncStorage.setItem('refreshToken', refreshToken);
 
-          axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-          return axios(originalRequest);
+          return api(originalRequest);
         }
 
+        // Caso deu erro ao recarregar o token ir para página de login
+        await AsyncStorage.setItem('accessToken', '');
+        NavigationService.navigate('Login');
         return null;
+      })
+      .catch(async () => {
+        await AsyncStorage.setItem('accessToken', '');
+        NavigationService.navigate('Login');
+        return null;
+      })
+      .finally(() => {
+        obtendoToken = false;
       });
   }
 
